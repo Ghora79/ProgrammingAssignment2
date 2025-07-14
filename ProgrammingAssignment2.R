0a1ed13de77fadf7889d463fdc43859c3e39c349
Ghora <ujha@umassd.edu>
2025-07-14 18:43:55 -0400

    Revised commit with comments

--- ProgrammingAssignment2.R
+++ ProgrammingAssignment2.R
@@ -3,13 +3,18 @@ print("This file lives in GitHub")
 
 makeCacheMatrix <- function(x = matrix()) {
     inver <- NULL
+    # set the value of the matrix
     set <- function(y) {
         x <<- y
         inver <<- NULL
     }
+    # get the value of the matrix
     get <- function() x
+    # set the value of the inverse
     setinverse <- function(inverse) inver <<- inverse
+    # get the value of the inverse
     getinverse <- function() inver
+    # return
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -19,12 +24,14 @@ M$set(matrix(rnorm(16), nrow = 4))
 M$get()
 
 cacheSolve <- function(x, ...) {
+    
     inver <- x$getinverse()
     if(!is.null(inver)) {
         message("getting cached data")
-        return(inver)
+        return(inver) # return ends the function
     }
     data <- x$get()
+    # calculates the inverse and stores it as inv
     inver <- solve(data, ...)
     x$setinverse(inver)
     inver
